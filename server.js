1b18b082653a07232f8fea28a2a278462f29fea8
ArtemKos2022 <110256138+ArtemKos2022@users.noreply.github.com>
2023-05-17 19:31:25 +0300

    DB_HOST

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const mongoose = require("mongoose");
 
 const app = require('./app');
-// const { error } = require('./schema/contacts');
-// qS3Pm67JbgmPeX6D
+const { DB_HOST, PORT = 3000 } = process.env;
 
-const DB_HOST = "mongodb+srv://Artem:qS3Pm67JbgmPeX6D>@cluster0.uct32pf.mongodb.net/db-contacts?retryWrites=true&w=majority"
-mongoose.connect(DB_HOST)
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    console.log("Database connection successful");
+
+    app.listen(PORT, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
   })
   .catch(error => {
     console.log(error.message);
     process.exit(1);
   });
-
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000")
-// })
