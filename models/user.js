bba372460a9cb47845ec452b1c31b305c13d465c
ArtemKos2022 <110256138+ArtemKos2022@users.noreply.github.com>
2023-06-04 12:45:29 +0300

    add fille

--- models/user.js
+++ models/user.js
@@ -2,28 +2,38 @@ const { Schema, model } = require("mongoose");
 const { handleMongooseError } = require("../helpers");
 const { regexpEmail } = require("../schemas/regexps")
 
-const userSchema = new Schema({
-  password: {
-    type: String,
-    required: [true, 'Set password for user'],
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Set password for user'],
+    },
+    email: {
+      type: String,
+      match: regexpEmail,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter"
+    },
+    token: {
+      type: String,
+      default: "",
+    },
+    avatarURL: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    match: regexpEmail,
-    required: [true, 'Email is required'],
-    unique: true,
-  },
-  subscription: {
-    type: String,
-    enum: ["starter", "pro", "business"],
-    default: "starter"
-  },
-  token: {
-    type: String,
-    default: "",
-  }
-}, { versionKey: false, timestamps: true });
-
+  { versionKey: false, timestamps: true }
+);
 
 userSchema.post("save", handleMongooseError);
 
